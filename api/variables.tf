566a5e792229fe17b803042c2b6a8bd4aa931961
vincentminutella <157165917+vincentminutella@users.noreply.github.com>
2024-02-16 17:18:58 -0800

    Update variables.tf

--- api/variables.tf
+++ api/variables.tf
@@ -1,15 +1,3 @@
-variable "domain_name" {
-    type = string 
-    description = "domain of the api gateway"
-    default = "brosona-api-test.net"
-}
-
-variable "cert" {
-    type = string
-    description = "arn of domain cert"
-    default = "arn:aws:acm:us-west-1:730335346805:certificate/f18fdec0-3247-4ccc-8285-0d196c9a6a59"
-}
-
 variable "sqs_arn" {
     type = string
     description = "arn of target queue"
